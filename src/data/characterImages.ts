ba95cd018a80556ebb38f370920204e1ef2fe6a0
小泉 博史 <koizumi@ad.tanabeconsulting.co.jp>
2026-06-22 20:15:43 +0900

    Add more matching characters

--- src/data/characterImages.ts
+++ src/data/characterImages.ts
@@ -3,7 +3,10 @@ export const characterImages = {
   sakura: require('../../assets/images/characters/sakura.png'),
   misaki: require('../../assets/images/characters/misaki.png'),
   nana: require('../../assets/images/characters/nana.png'),
+  aoi: require('../../assets/images/characters/aoi.png'),
+  rina: require('../../assets/images/characters/rina.png'),
+  haruka: require('../../assets/images/characters/haruka.png'),
+  emi: require('../../assets/images/characters/emi.png'),
 } as const;
 
 export type CharacterImageId = keyof typeof characterImages;
-
