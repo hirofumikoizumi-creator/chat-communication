ff590d3ecc514fce4af140c13207d800344f6dcf
小泉 博史 <koizumi@ad.tanabeconsulting.co.jp>
2026-06-23 03:54:00 +0900

    Use distinct images for added characters

--- src/data/characterImages.ts
+++ src/data/characterImages.ts
@@ -3,10 +3,10 @@ export const characterImages = {
   sakura: require('../../assets/images/characters/sakura.png'),
   misaki: require('../../assets/images/characters/misaki.png'),
   nana: require('../../assets/images/characters/nana.png'),
-  aoi: require('../../assets/images/characters/aoi.png'),
-  rina: require('../../assets/images/characters/rina.png'),
-  haruka: require('../../assets/images/characters/haruka.png'),
-  emi: require('../../assets/images/characters/emi.png'),
+  aoi: require('../../assets/images/characters/aoi-v2.png'),
+  rina: require('../../assets/images/characters/rina-v2.png'),
+  haruka: require('../../assets/images/characters/haruka-v2.png'),
+  emi: require('../../assets/images/characters/emi-v2.png'),
 } as const;
 
 export type CharacterImageId = keyof typeof characterImages;
